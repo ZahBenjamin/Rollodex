589df7735ac0c2c8869ec858300e9a32702c68ca
ZahBenjamin <bz@benzah.com>
2021-11-18 20:45:15 -0500

    User uniqueness and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_190558) do
+ActiveRecord::Schema.define(version: 2021_11_19_013458) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
